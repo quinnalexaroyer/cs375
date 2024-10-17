cd3633f628609e35262fea660e0877e7dcaafb6b
qroyer-cs5513 <q3x14159@gmail.com>
2024-10-17 16:15:12 -0700

    remove commented code from experimental cube

--- Assignment-3/ExperimentalCube.js
+++ Assignment-3/ExperimentalCube.js
@@ -34,7 +34,7 @@ class ExperimentalCube {
         const co = new Float32Array([ 0.5,  0.5,  0.5,    0.5,  0.5, -0.5,    0.5, -0.5,  0.5,
                                       0.5, -0.5, -0.5,   -0.5,  0.5,  0.5,   -0.5,  0.5, -0.5,
                                      -0.5, -0.5,  0.5,   -0.5, -0.5, -0.5]);
-        const indexList = new Int8Array([1, 0, 3, 2, 7, 6, 4, 2, 0, 0, 4, 1, 5, 3, 7, 7, 5, 4]); //, 2, 6, 4, 5, 1, 3, 7, 4, 6, 2, 3
+        const indexList = new Int8Array([1, 0, 3, 2, 7, 6, 4, 2, 0, 0, 4, 1, 5, 3, 7, 7, 5, 4]);
         const color = new Float32Array([1,1,1, 1,0,0, 1,1,0, 1,0.5,0, 0,1,1, 0.5,1,0, 0,1,0, 0,0,0]);
         let aPosition = new Attribute(gl, program, "aPosition", co, 3, gl.FLOAT);
         let indices = new Indices(gl, indexList);
