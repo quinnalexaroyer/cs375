856e6e371c5ca3762dce98eb527470636b175f11
qroyer-cs5513 <q3x14159@gmail.com>
2024-10-19 14:56:15 -0700

    modify experimental cube to avoid zero area triangles

--- Assignment-3/ExperimentalCube.js
+++ Assignment-3/ExperimentalCube.js
@@ -34,7 +34,7 @@ class ExperimentalCube {
         const co = new Float32Array([ 0.5,  0.5,  0.5,    0.5,  0.5, -0.5,    0.5, -0.5,  0.5,
                                       0.5, -0.5, -0.5,   -0.5,  0.5,  0.5,   -0.5,  0.5, -0.5,
                                      -0.5, -0.5,  0.5,   -0.5, -0.5, -0.5]);
-        const indexList = new Int8Array([1, 0, 3, 2, 7, 6, 4, 2, 0, 0, 4, 1, 5, 3, 7, 7, 5, 4]);
+        const indexList = new Int8Array([4, 5, 6, 7, 3, 5, 1, 0, 3, 2, 6, 0, 4, 5]);
         const color = new Float32Array([1,1,1, 1,0,0, 1,1,0, 1,0.5,0, 0,1,1, 0.5,1,0, 0,1,0, 0,0,0]);
         let aPosition = new Attribute(gl, program, "aPosition", co, 3, gl.FLOAT);
         let indices = new Indices(gl, indexList);
